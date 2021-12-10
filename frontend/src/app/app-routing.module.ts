759ed5e7bcbc1c00f3e9e23e582b0a637e850428
Camila <ucami98@gmail.com>
2021-12-09 22:17:40 -0500

    Update componentes navbar,inicio y actividades en el front

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { ActividadesComponent } from './components/actividades/actividades.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+const routes: Routes = [
+  {path:"", component: InicioComponent},
+  {path:"inicio", component: InicioComponent},
+  {path:"actividades", component: ActividadesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
